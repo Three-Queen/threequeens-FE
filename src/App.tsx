3cd16dc9dd1a624905433956e14460562fe41b46
FirdanAja <159457668+FirdanAja@users.noreply.github.com>
2026-06-10 19:45:44 +0700

    first commit setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <div>
+      <h1 
+        className="font-bold text-red-900 text-4xl"
+      >
+        HELOO KONTOLLLLL
+      </h1>
+      <p className="text-blue-500 mt-4">Testing Tailwind</p>
+    </div>
+  )
+}
+
+export default App
